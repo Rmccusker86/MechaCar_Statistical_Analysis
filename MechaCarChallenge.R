18bb9ce38215ede4706f88593732447484ead880
Ryan M <Rjmccusker86@gmail.com>
2021-12-19 22:20:22 -0500

    yes

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,11 @@ lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = 
 
 #p-value and r-squared
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mech_mpg_df))
+
+
+# Suspension Coil Analysis
+suspension_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+# create summary dataframe
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+# create lot summary
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MechaCar_Statistical_Analysis
+
+##Overview
+
+We have been tasking with doing an analytical review to try and show why the new MechaCar is suffering from production troubles. The desire goal of the project is this:
+  * discover which variables determine the cars MPG's
+  * summarize the stats on the PSI of the suspension coils
+  * use the data to determine if the statistics are different across the lots
+  * come up with a study to compare the performance to vehicles from other automakers.
+  
+##Results
